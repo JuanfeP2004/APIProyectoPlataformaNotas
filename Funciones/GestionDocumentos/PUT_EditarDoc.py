69b415dfd597028f568ad91c11b869e337a6d050
JuanfeP2004 <juanf.ramirezc@upb.edu.co>
2024-10-19 03:30:35 -0500

    Se agrego la funcion de editar un documento

--- Funciones/GestionDocumentos/PUT_EditarDoc.py
+++ Funciones/GestionDocumentos/PUT_EditarDoc.py
@@ -1,7 +1,8 @@
 import os
 import sys
-import uuid
 import json
+from bson.objectid import ObjectId
+from datetime import datetime
 
 ruta_archivo = os.path.dirname( __file__ )
 ruta_config = os.path.join( ruta_archivo, '..')
@@ -9,37 +10,71 @@ sys.path.append( ruta_config )
 from Servicios.Storage import storage, contenedor
 
 from flask import Blueprint, request, jsonify
-# Falta importar la funcion de base de datos
 
 PUT_EditarDoc = Blueprint('PUT_EditarDoc', __name__)
 
 @PUT_EditarDoc.route('/EditarDocumento', methods=['PUT'])
 def EditarDocumento():
     try:
-        if "file" not in request.files:
-            return jsonify({'error': 'No se proporcionó un archivo'}), 400
+        from app import mongo
 
-        metadatos = request.form.get('json')
+        #if "file" not in request.files:
+        #    return jsonify({'error': 'No se proporcionó un archivo'}), 400
+        if "json" not in request.form:
+            return jsonify({'error': 'No se proporcionó un json'}), 400
+
+        metadatos = request.form['json']
 
         if not metadatos:
             return jsonify({'error': 'No se proporcionó un json'}), 400
+    
+        datos_json = json.loads(metadatos)
 
-        datos = request.form['json']      
-        archivo = request.files["file"]
-        datos_json = json.loads(datos)
+        id = ObjectId(datos_json['id'])
+        titulo = datos_json['titulo']
+        usuario_id = ObjectId(datos_json['usr_id'])
+        tipo = str.upper(datos_json['tipo'])
+        materia = str.upper(datos_json['materia'])
+        fecha = datetime.now()
 
-        nombre = datos_json['nombre']
-        tipo = datos_json['tipo']
-        materia = datos_json['materia']
-        universidad = datos_json['universidad']
 
-        unique_id = str(uuid.uuid4())
-        ubicacion = unique_id + '_' + archivo.filename
+        coleccion_usr = mongo.db['Usuarios']
+        coleccion_doc = mongo.db['documentos']
+        materias = mongo.db['materias']
 
-        cliente = storage.get_blob_client(container=contenedor, blob=ubicacion)
-        cliente.upload_blob(archivo)
+        existe_documento = coleccion_doc.find_one({'_id': id})
 
-        return 'Se subio el archivo correctamente'
+        if not existe_documento:
+            return jsonify({'error': 'No existe ese documento'}), 400
+
+        existe_usuario = coleccion_usr.find_one({"_id": usuario_id})
+        val_mat = materias.find_one({"nombre": materia})
+
+        if titulo in (None, ''):
+            return jsonify({'error': 'El titulo no puede estar en blanco o vacio'}), 400
+        if tipo in (None, ''):
+            return jsonify({'error': 'El tipo de documento no puede estar en blanco o vacio'}), 400
+        if not existe_usuario:
+            return jsonify({'error': 'No existe ese usuario'}), 400
+        elif not val_mat:
+            return jsonify({'error': 'No existe esa materia'}), 400
+
+        if "file" in request.files:
+            archivo = request.files["file"]
+            cliente = storage.get_blob_client(container=contenedor, blob=existe_documento['archivo'])
+       
+            cliente.upload_blob(archivo, overwrite=True)
+
+        documento = {
+            "titulo": titulo,
+            "materia_id": val_mat['_id'],
+            "tipo_documento": tipo,
+            "fecha_modificacion": fecha,
+        }
+
+        resultado = mongo.db.documentos.update_one({"_id": id}, {"$set": documento})
+
+        return jsonify({"ok": 'Se actualizo el documento correctamente'}), 200
     
     except Exception as e:
         return jsonify({"error": str(e)}), 500
--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from Funciones.Servicios.BaseDatos import Config
 #from Funciones.Calificaciones.POST_AgregarCom import POST_AgregarCom
 
 from Funciones.GestionDocumentos.POST_SubirDoc import POST_SubirDoc
+from Funciones.GestionDocumentos.PUT_EditarDoc import PUT_EditarDoc
 
 from Funciones.GestionUsuarios.POST_CrearUsuario import POST_CrearUsuario
 from Funciones.GestionUsuarios.PUT_EditarUsuario import PUT_EditarUsuario
@@ -33,6 +34,7 @@ mongo = PyMongo(app)
 #app.register_blueprint(POST_AgregarCom)
 
 app.register_blueprint(POST_SubirDoc)
+app.register_blueprint(PUT_EditarDoc)
 
 app.register_blueprint(POST_CrearUsuario)
 app.register_blueprint(PUT_EditarUsuario)
